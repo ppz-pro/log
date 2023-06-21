64458c84dc387ca394c5996cbf0634d3cbec38cf
ppz <372301467@qq.com>
2023-06-21 13:59:07 +0800

    fix: 移除 @ppzp/meta

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-require('@ppzp/meta')
-
-const now = () => new Date().format()
+const config = {
+  now: () => new Date()
+}
 
 module.exports = {
+  config,
   debug() {
-    console.debug(`[debug ${now()}]`, ...arguments)
+    console.debug(`[debug ${config.now()}]`, ...arguments)
   },
   info() {
-    console.log(`[info ${now()}]`, ...arguments)
+    console.log(`[info ${config.now()}]`, ...arguments)
   },
-//   warn() { // 如果一件事做得不好，应直接判定为 error
-//     console.log(`[warn ${new Date()}]`, ...arguments)
-//   },
+  warn() { // 如果一件事做得不好，应直接判定为 error，除非，，，
+    console.warn(`[warn ${config.now()}]`, ...arguments)
+  },
   error() {
-    console.log(`[error ${now()}]`, ...arguments)
+    console.error(`[error ${config.now()}]`, ...arguments)
   }
 }
